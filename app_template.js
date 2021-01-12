5a01f3a5ba800a69a8b39fdbadb6d46269278df5
danwristband <dan.akkaraphan@gmail.com>
2021-01-12 14:01:29 +0700

    Update app_template.js

--- app_template.js
+++ app_template.js
@@ -1,1 +1,14 @@
 //your code here!
+const num=document.getElementById('input-number').value;
+const buttonnum = document.getElementById('button-compute');
+const printt=document.querySelector('p')
+
+buttonnum.onClick=function(){
+     var numin = input.value;
+     input.value="";
+     var textnum =document.createElement('p');
+     textnum.textContent= 620610821-numin;
+     printt.appendChild(textnum);
+     console.log(numin);
+     input.focus();
+}
